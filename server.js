89c71dab797e87fbae34b50b48ad964c6ca71aa6
Igor Boyko <iboyko@gmail.com>
2022-12-20 11:38:18 -0800

    added server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Here we're setting the views directory to be ./views
+// thereby letting the app know where to find the template files
+app.set('views', './views');
+
+//Here we're setting the default engine to be ejs
+// note we don't need to require it, express will do that for us
+app.set('view engine', 'ejs');
+
+//Now instead of using res.send we can use 
+// res.render to send the output of the template by filename
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080');
+});
