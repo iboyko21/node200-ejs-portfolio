0e412748d64d47634db01d7da66ccae5f6eb236d
Igor Boyko <iboyko@gmail.com>
2022-12-20 12:01:43 -0800

    basic working setup

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+app.post('/thanks', (req, res) => {
+    res.render('thanks', { contact: req.body })
+})
 
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080');
